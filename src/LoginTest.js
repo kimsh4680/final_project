e64e36889d18a4ba38bb2d56cf5211bd1ef7bd6e
Sung-hyoun <jqwesasd7703@naver.com>
2021-10-29 17:13:41 +0900

    test

--- src/LoginTest.js
+++ src/LoginTest.js
@@ -12,7 +12,7 @@ export default function LoginTest(){
 
     const initializeNaverLogin = () => {
         const naverLogin = new naver.LoginWithNaverId({
-          clientId:"GFRk_SJiZVzSpToWr01V",
+          clientId:"11GFRk_SJiZVzSpToWr01V",
           callbackUrl:"http://localhost:3000/#/", 
           isPopup: false, // popup 형식으로 띄울것인지 설정
           loginButton: { color: 'green', type: 3, height: '30' }, //버튼의 스타일, 타입, 크기를 지정
