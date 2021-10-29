d1028030a9051741459f9c38f39531b0f658dc72
kimsh4680 <kimsh4680@gmail.com>
2021-10-29 22:06:00 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import LoginTest from "./LoginTest";
 import Home from "./Home";
-import {Route } from 'react-router-dom';
+import { Route } from "react-router-dom";
 import JoinTest from "./JoinTest";
+import "bootstrap/dist/css/bootstrap.css";
 
-export default function App(){
-  return(
+export default function App() {
+  return (
     <>
-      <Route exact path="/" component={Home}/>
-      <Route path="/join" component={JoinTest}/>
-      <Route path="/login" component={LoginTest}/>
+      <Route exact path="/" component={Home} />
+      <Route path="/join" component={JoinTest} />
+      <Route path="/login" component={LoginTest} />
     </>
   );
 }
--- src/LoginTest.js
+++ src/LoginTest.js
@@ -1,81 +1,123 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
-import { Redirect, useLocation } from 'react-router';
-const {naver} = window;
-  
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Redirect, useLocation } from "react-router";
+import { Form, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./LoginTest.css";
 
+const { naver } = window;
 
-export default function LoginTest(){
-    const [user,setUser] = useState({});
-    const [loginCheck,setLoginCheck] = useState(false);
-    const location = useLocation();
+export default function LoginTest() {
+  const [user, setUser] = useState({});
+  const [loginCheck, setLoginCheck] = useState(false);
+  const location = useLocation();
 
-    const initializeNaverLogin = () => {
-        const naverLogin = new naver.LoginWithNaverId({
-          clientId:"GFRk_SJiZVzSpToWr01V",
-          callbackUrl:"http://localhost:3000/#/", 
-          isPopup: false, // popup 형식으로 띄울것인지 설정
-          loginButton: { color: 'green', type: 3, height: '30' }, //버튼의 스타일, 타입, 크기를 지정
-        });
-        naverLogin.init();
-    };
-    
-    const getNaverToken = () => {
-        if (!location.hash) return;
-        const token = location.hash.split('=')[1].split('&')[0];
-        console.log(token);
-    };
+  const initializeNaverLogin = () => {
+    const naverLogin = new naver.LoginWithNaverId({
+      clientId: "GFRk_SJiZVzSpToWr01V",
+      callbackUrl: "http://localhost:3000/#/",
+      isPopup: false, // popup 형식으로 띄울것인지 설정
+      loginButton: { color: "green", type: 3, height: "94" }, //버튼의 스타일, 타입, 크기를 지정
+    });
+    naverLogin.init();
+  };
 
-    function onChange(e){
-        const name = e.target.name;
-        if(name === "id"){
-            setUser({...user,userId:e.target.value});
-        }else if(name === "password"){
-            setUser({...user,userPassword:e.target.value});
-        }
+  const getNaverToken = () => {
+    if (!location.hash) return;
+    const token = location.hash.split("=")[1].split("&")[0];
+    console.log(token);
+  };
+
+  function onChange(e) {
+    const name = e.target.name;
+    if (name === "id") {
+      setUser({ ...user, userId: e.target.value });
+    } else if (name === "password") {
+      setUser({ ...user, userPassword: e.target.value });
     }
+  }
 
-    function onSubmit(e){
-        e.preventDefault();
-        axios.post(`http://localhost:8081/user/login`,user)
-        .then(function(response){
-            console.log('성공');
-            if(response.data.userId !== ""){
-                window.sessionStorage.setItem("user", JSON.stringify(response.data));
-                setLoginCheck(!loginCheck);
-            }else{
-                alert("로그인 실패")
-            }
-        })
-        .catch(function(error){
-            console.log('실패');
-        });
-      }
+  function onSubmit(e) {
+    e.preventDefault();
+    axios
+      .post(`http://localhost:8081/user/login`, user)
+      .then(function (response) {
+        console.log("성공");
+        if (response.data.userId !== "") {
+          window.sessionStorage.setItem("user", JSON.stringify(response.data));
+          setLoginCheck(!loginCheck);
+        } else {
+          alert("로그인 실패");
+        }
+      })
+      .catch(function (error) {
+        console.log("실패");
+      });
+  }
 
-      useEffect(()=>{
-        console.log("useEffect실행");
-        initializeNaverLogin();
-        getNaverToken();
-      },[]);
+  useEffect(() => {
+    console.log("useEffect실행");
+    initializeNaverLogin();
+    getNaverToken();
+  }, []);
 
-    return(
-        <>    
-            {
-                sessionStorage.getItem("user") !== null ? 
-                <Redirect to="/"></Redirect>
-                :
-                <div>
-                    <h1>로그인 페이지</h1>
-                    <form onSubmit={onSubmit}> 
-                        아이디
-                        <input type = "text" name="id" onChange={onChange}></input><br/>
-                        비밀번호
-                        <input type = "password" name="password" onChange={onChange}></input>
-                        <input type="submit" value="로그인"></input>
-                    </form>
-                    <div id="naverIdLogin"></div>
-                </div>
-            }
-        </>
-    );
+  return (
+    <>
+      <style type="text/css">
+        {`
+                Form.Group{
+                    margin-bottom: 15px;
+                }
+                `}
+      </style>
+      {sessionStorage.getItem("user") !== null ? (
+        <Redirect to="/"></Redirect>
+      ) : (
+        <div className="loginform">
+          <h1>국비 위키</h1>
+
+          <br />
+          <Form onSubmit={onSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <br />
+              <input
+                className="input_id"
+                type="text"
+                name="id"
+                placeholder="이메일 주소"
+                onChange={onChange}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-4" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <br />
+              <input
+                className="input_password"
+                type="password"
+                name="password"
+                placeholder="비밀번호"
+                onChange={onChange}
+              />
+            </Form.Group>
+
+            <div className="d-grid gap-2 mb-4">
+              <Button variant="success" type="submit" size="lg">
+                로그인
+              </Button>
+            </div>
+
+            <div>
+              <div className="horizontal">
+                <span>or</span>
+              </div>
+            </div>
+          </Form>
+
+          <div id="naverIdLogin"></div>
+        </div>
+      )}
+    </>
+  );
 }
